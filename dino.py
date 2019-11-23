1172d58b8b1528e73794d51053dccd867f1f7790
matt <msharp9@outlook.com>
2019-11-23 13:07:49 -0700

    Created a basic agent that mostly works, imported mss to resolve latency issues

--- dino.py
+++ dino.py
@@ -1,17 +1,32 @@
-from PIL import ImageOps
+from PIL import Image, ImageOps
 import pyautogui
 import time
 import numpy as np
 import random
 
+import sys
+from mss import mss
+import mss.tools as tools
+
+pyautogui.FAILSAFE = True
+pyautogui.PAUSE = 0.1
+
 class Bot:
     """Bot for playing Chrome dino run game"""
     def __init__(self):
-        self.area = pyautogui.locateOnScreen('dino_start.png', confidence=0.9)
+        self.area = pyautogui.locateOnScreen('dino_start.png', confidence=0.9, grayscale=True)
+        if not self.area:
+            print("Game area could not be found. Please load up a new game at http://www.trex-game.skipser.com/.")
+            sys.exit()
         self.restart_coords = pyautogui.center(self.area)
+        self.observation_area = {"top": self.area.top + 110,
+            "left": self.area.left +130, "width": 140, "height": 100}
+        self.mss = mss()
 
     def restart(self):
         pyautogui.click(self.restart_coords)
+        time.sleep(0.05)
+        pyautogui.press('space')
 
     def duck(self):
         pyautogui.keyDown('down')
@@ -19,9 +34,11 @@ class Bot:
 
     def jump(self):
         pyautogui.keyUp('down')
-        pyautogui.keyDown('space')
-        time.sleep(0.05)
-        pyautogui.keyUp('space')
+        pyautogui.press('space')
+        # press is clean but is affected by the PAUSE attribute
+        # pyautogui.keyDown('space')
+        # time.sleep(0.05)
+        # pyautogui.keyUp('space')
 
     def short_jump(self):
         pyautogui.keyUp('down')
@@ -34,15 +51,17 @@ class Bot:
 
     def detection_area(self):
         """
-        Checks the area to have obstacles
-        :return: float
+        Area right in front of dino.
         """
-        image = pyautogui.screenshot(region=self.area)
-        # print(self.area)
-        # image.show()
-        gray_img = ImageOps.grayscale(image)
-        arr = np.array(gray_img.getcolors())
-        print(arr.mean())
+        # img = pyautogui.screenshot(region=self.observation_area)
+        sct = self.mss.grab(self.observation_area)
+        # tools.to_png(sct.rgb, sct.size, output='test.png')
+        img = Image.frombytes("RGB", sct.size, sct.bgra, "raw", "BGRX")
+        gray_img = ImageOps.grayscale(img)
+        arr = np.array(gray_img)
+        return arr
+
+    def mean_pixel(self, arr):
         return arr.mean()
 
     def random_agent(self):
@@ -58,8 +77,17 @@ class Bot:
             else:
                 self.walk()
 
-    def
+    def basic_agent(self):
+        BA_THRESHOLD = 241
+        # start = time.time()
+        self.restart()
+        while True:
+            # print(time.time() - start)
+            if self.mean_pixel(self.detection_area()) < BA_THRESHOLD:
+                self.jump()
+                # self.short_jump()
 
 if __name__ == "__main__":
     bot = Bot()
-    bot.random_agent()
+    # bot.random_agent()
+    bot.basic_agent()
